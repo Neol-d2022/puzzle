59309ce4e3112090f6c59a22bc3b90958f70cbca
neol-d2022 <neol_d2022@outlook.com>
2017-01-29 02:59:43 +0800

    Linear Conflict

--- core.c
+++ core.c
@@ -5,14 +5,14 @@
 unsigned int CalcDis(PLATE *current, const PLATE *goal, unsigned int **buffers)
 {
     unsigned int *p = buffers[0], *d = buffers[1];
-    unsigned int i, j, k, x[2];
+    unsigned int i, j, k, l;
 
     for (i = 0; i < PUZZLE_SIZE * PUZZLE_SIZE; i += 1)
     {
         if ((current->s)[i] == 0)
         {
             p[i] = 0;
-            d[i] = 16;
+            d[i] = 32;
             continue;
         }
         for (j = 0; j < PUZZLE_SIZE * PUZZLE_SIZE; j += 1)
@@ -35,7 +35,7 @@ unsigned int CalcDis(PLATE *current, const PLATE *goal, unsigned int **buffers)
                 }
                 else if (i % PUZZLE_SIZE == j % PUZZLE_SIZE)
                 {
-                    if (i / PUZZLE_SIZE < j / PUZZLE_SIZE)
+                    if (i / PUZZLE_SIZE > j / PUZZLE_SIZE)
                     {
                         d[i] = 8; //Up
                     }
@@ -44,6 +44,8 @@ unsigned int CalcDis(PLATE *current, const PLATE *goal, unsigned int **buffers)
                         d[i] = 4; //Down
                     }
                 }
+                else
+                    d[i] = 16;
                 break;
             }
         }
@@ -51,7 +53,7 @@ unsigned int CalcDis(PLATE *current, const PLATE *goal, unsigned int **buffers)
             return -1;
     }
 
-    for (i = 0; i < PUZZLE_SIZE * PUZZLE_SIZE; i += 1)
+    /*for (i = 0; i < PUZZLE_SIZE * PUZZLE_SIZE; i += 1)
     {
         if ((current->s)[i] == 0)
             continue;
@@ -60,9 +62,7 @@ unsigned int CalcDis(PLATE *current, const PLATE *goal, unsigned int **buffers)
         {
             if (d[i] == 4)
             {
-                if (d[i + PUZZLE_SIZE] == 8)
-                    p[i] += 1;
-                else if (d[i + PUZZLE_SIZE] == 0)
+                if (d[i + PUZZLE_SIZE] == 0)
                     p[i] += 2;
             }
         }
@@ -70,9 +70,7 @@ unsigned int CalcDis(PLATE *current, const PLATE *goal, unsigned int **buffers)
         {
             if (d[i] == 8)
             {
-                if (d[i - PUZZLE_SIZE] == 4)
-                    p[i] += 1;
-                else if (d[i - PUZZLE_SIZE] == 0)
+                if (d[i - PUZZLE_SIZE] == 0)
                     p[i] += 2;
             }
         }
@@ -80,16 +78,12 @@ unsigned int CalcDis(PLATE *current, const PLATE *goal, unsigned int **buffers)
         {
             if (d[i] == 4)
             {
-                if (d[i + PUZZLE_SIZE] == 8)
-                    p[i] += 1;
-                else if (d[i + PUZZLE_SIZE] == 0)
+                if (d[i + PUZZLE_SIZE] == 0)
                     p[i] += 2;
             }
             if (d[i] == 8)
             {
-                if (d[i - PUZZLE_SIZE] == 4)
-                    p[i] += 1;
-                else if (d[i + PUZZLE_SIZE] == 0)
+                if (d[i - PUZZLE_SIZE] == 0)
                     p[i] += 2;
             }
         }
@@ -97,9 +91,7 @@ unsigned int CalcDis(PLATE *current, const PLATE *goal, unsigned int **buffers)
         {
             if (d[i] == 1)
             {
-                if (d[i + 1] == 2)
-                    p[i] += 1;
-                else if (d[i + 1] == 0)
+                if (d[i + 1] == 0)
                     p[i] += 2;
             }
         }
@@ -107,9 +99,7 @@ unsigned int CalcDis(PLATE *current, const PLATE *goal, unsigned int **buffers)
         {
             if (d[i] == 2)
             {
-                if (d[i - 1] == 1)
-                    p[i] += 1;
-                else if (d[i - 1] == 0)
+                if (d[i - 1] == 0)
                     p[i] += 2;
             }
         }
@@ -117,17 +107,48 @@ unsigned int CalcDis(PLATE *current, const PLATE *goal, unsigned int **buffers)
         {
             if (d[i] == 1)
             {
-                if (d[i + 1] == 2)
-                    p[i] += 1;
-                else if (d[i + 1] == 0)
+                if (d[i + 1] == 0)
                     p[i] += 2;
             }
             if (d[i] == 2)
             {
-                if (d[i - 1] == 1)
-                    p[i] += 1;
-                else if (d[i - 1] == 0)
+                if (d[i - 1] == 0)
                     p[i] += 2;
+            }
+        }
+    }*/
+
+    l = 0;
+    for (i = 0; i < PUZZLE_SIZE; i += 1)
+    {
+        for (j = 0; j < PUZZLE_SIZE; j += 1)
+        {
+            for (k = j + 1; k < PUZZLE_SIZE; k += 1)
+            {
+                if (d[(i * PUZZLE_SIZE) + j] == 1 && d[(i * PUZZLE_SIZE) + k] == 2)
+                {
+                    l += 2;
+                }
+                if (d[(i * PUZZLE_SIZE) + j] == 1 && d[(i * PUZZLE_SIZE) + k] == 0 && k - j < p[(i * PUZZLE_SIZE) + j])
+                {
+                    l += 2;
+                }
+                if (d[(i * PUZZLE_SIZE) + j] == 0 && d[(i * PUZZLE_SIZE) + k] == 2 && k - j < p[(i * PUZZLE_SIZE) + k])
+                {
+                    l += 2;
+                }
+                if (d[(j * PUZZLE_SIZE) + i] == 4 && d[(k * PUZZLE_SIZE) + i] == 8)
+                {
+                    l += 2;
+                }
+                if (d[(j * PUZZLE_SIZE) + i] == 4 && d[(k * PUZZLE_SIZE) + i] == 0 && k - j < p[(j * PUZZLE_SIZE) + i])
+                {
+                    l += 2;
+                }
+                if (d[(j * PUZZLE_SIZE) + i] == 0 && d[(k * PUZZLE_SIZE) + i] == 8 && k - j < p[(k * PUZZLE_SIZE) + i])
+                {
+                    l += 2;
+                }
             }
         }
     }
@@ -137,5 +158,6 @@ unsigned int CalcDis(PLATE *current, const PLATE *goal, unsigned int **buffers)
     {
         k += p[i];
     }
+    k += l;
     return k;
 }
